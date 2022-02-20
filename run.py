2d368a1322e8cacac95682ae739fa7ef28c4a177
Zutatensuppe <oo.para@gmail.com>
2022-02-20 18:55:27 +0100

    make sr good/bad only color for 3 sec

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Optional
 import config
 
@@ -41,22 +42,45 @@ async def _turn_on(builder: PilotBuilder, light: Optional[str] = config.default_
     await bulb.turn_on(builder)
 
 
+last_manual_msg = "!rgb 255 180 70"
+
+
+async def redo_last_manual_msg():
+    global last_manual_msg
+    log_debug(f"redo_last_manual_msg: {last_manual_msg}")
+    if last_manual_msg:
+        await _handle_message(last_manual_msg)
+
+
 async def _handle_message(msg: str):
+    global last_manual_msg
+
     lower_msg_full = msg.lower()
     if not lower_msg_full.startswith("!"):
         return
 
     if lower_msg_full.startswith("!sr bad"):
-        lower_msg_full = "!rgb 255 0 0"
+        await _turn_on(PilotBuilder(rgb=(255, 0, 0)))
+        loop = asyncio.get_event_loop()
+        loop.call_later(3.0, loop.create_task, redo_last_manual_msg())
+        return
+
     elif lower_msg_full.startswith("!sr good"):
-        lower_msg_full = "!rgb 0 255 0"
+        await _turn_on(PilotBuilder(rgb=(0, 255, 0)))
+        loop = asyncio.get_event_loop()
+        loop.call_later(3.0, loop.create_task, redo_last_manual_msg())
+        return
+
+    # Above line of codes will not execute some_fn call as timer is cleared before 3 seconds
 
     lower_msg = lower_msg_full[1:]
     if lower_msg in config.named_colors:
+        last_manual_msg = msg
         await _turn_on(
             PilotBuilder(
                 rgb=config.named_colors[lower_msg]["rgb"],
                 brightness=config.named_colors[lower_msg]["brightness"],
+                speed=1,
             )
         )
     elif lower_msg.startswith("rgb "):
@@ -65,25 +89,31 @@ async def _handle_message(msg: str):
         g = int(g)
         b = int(b)
         if r >= 0 and r <= 255 and g >= 0 and g <= 255 and b >= 0 and b <= 255:
-            await _turn_on(PilotBuilder(rgb=(r, g, b)))
+            last_manual_msg = msg
+            await _turn_on(PilotBuilder(rgb=(r, g, b), speed=1))
     elif lower_msg.startswith("brightness "):
         brightness = lower_msg[11:]
         brightness = int(brightness)
         if brightness >= 0 and brightness <= 255:
-            await _turn_on(PilotBuilder(brightness=brightness))
+            last_manual_msg = msg
+            await _turn_on(PilotBuilder(brightness=brightness, speed=1))
     elif lower_msg.startswith("scene "):
         scene = lower_msg[6:]
         if scene in config.named_scenes:
+            last_manual_msg = msg
             await _turn_on(
                 PilotBuilder(
                     rgb=config.named_scenes[scene]["rgb"],
                     brightness=config.named_scenes[scene]["brightness"],
+                    speed=1,
                 )
             )
             return
 
         try:
             scene_id = scenes.get_id_from_scene_name(scene.capitalize())
+            print(f"found scene_id {scene_id} for {scene.capitalize()}")
+            last_manual_msg = msg
             await _turn_on(PilotBuilder(scene=scene_id))
         except:
             # not found scene
@@ -135,6 +165,8 @@ async def event_message(ctx):
 
 
 async def main_task():
+    # await _handle_message("!rgb 255 255 255")
+    # await _handle_message("!brightness 255")
     topics = [
         pubsub.channel_points(users_oauth_token)[config.twitch_channel_id],
         pubsub.bits(users_oauth_token)[config.twitch_channel_id],
